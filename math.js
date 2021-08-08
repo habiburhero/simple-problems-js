7281f56801fefa15e046c11d89642ae718a169f1
habiburhero <habibur0087gmail.com>
2021-08-08 17:00:21 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,17 @@
+// const myNumber = 21.0001;
+// const outPut = Math.ceil(myNumber);
+// console.log(outPut);
+
+// const myNumber = 21.95;
+// const outPut = Math.floor(myNumber);
+// console.log(outPut);
+
+
+// const myNumber = 21.296;
+// const outPut = Math.round(myNumber);
+// console.log(outPut);
+
+
+const myNumber = 21.500;
+const outPut = Math.round(myNumber);
+console.log(outPut);
---
+++ max.js
@@ -0,0 +1,17 @@
+// const business = 450;
+// const minister = 350;
+// if (business > minister) {
+//     console.log('business person er pola is bigger');
+// }
+// else {
+//     console.log('misniter er pola is bigger');
+// }
+
+const business = 450;
+const minister = 550;
+if (business > minister) {
+    console.log('business person er pola is bigger');
+}
+else {
+    console.log('misniter er pola is bigger');
+}
